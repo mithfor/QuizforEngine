076aa3617e02aa637bb4cda9238ad8623f425872
Dmitriy Voronin <devoronin@gmail.com>
2023-06-29 21:34:34 +0300

    Adding scoring function

--- QuizforEngineTests/GameTest.swift
+++ QuizforEngineTests/GameTest.swift
@@ -10,9 +10,24 @@ import XCTest
 import QuizforEngine
 
 class GameTest: XCTestCase {
-    func test_startGame_answerOneOutOfTwoCorrectly_scores1() {
-        let router = RouterSpy()
-        startGame(questions: ["Q1", "Q2"],
+    let router = RouterSpy()
+    var game: Game<String, String, RouterSpy>!
+
+    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
+
+        self.game = startGame(questions: ["Q1", "Q2"],
+                  router: router,
+                  correctAnswers: ["Q1":"A1", "Q2":"A2" ])
+
+        router.answerCallback("wrong")
+        router.answerCallback("wrong")
+
+        XCTAssertEqual(router.routedResult!.score, 0)
+    }
+
+    func test_startGame_answerOneOutOfTwoCorrectly_scoresOne() {
+
+        self.game = startGame(questions: ["Q1", "Q2"],
                   router: router,
                   correctAnswers: ["Q1":"A1", "Q2":"A2" ])
 
@@ -20,5 +35,17 @@ class GameTest: XCTestCase {
         router.answerCallback("wrong")
 
         XCTAssertEqual(router.routedResult!.score, 1)
+    }
+
+    func test_startGame_answerTwoOutOfTwoCorrectly_scoresTwo() {
+
+        self.game = startGame(questions: ["Q1", "Q2"],
+                  router: router,
+                  correctAnswers: ["Q1":"A1", "Q2":"A2" ])
+
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+
+        XCTAssertEqual(router.routedResult!.score, 2)
     }
 }
