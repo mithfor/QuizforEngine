01fa93d0329f66915bbbd132f482072be7f404ec
Dmitriy Voronin <devoronin@gmail.com>
2023-07-06 10:08:46 +0300

    Replaced the Flow Router with QuizDelegate

--- QuizforEngine/Game.swift
+++ QuizforEngine/Game.swift
@@ -28,6 +28,24 @@ public func startGame<Question: Hashable,
     return Game(flow: flow)
 }
 
+@available(*, deprecated)
+private class QuizDelegateToRouterAdapter<R: Router>: QuizDelegate {
+
+    private let router: R
+
+    init(_ router: R) {
+        self.router = router
+    }
+
+    func handle(question: R.Question, answerCallback: @escaping (R.Answer) -> Void) {
+        router.routeTo(question: question, answerCallback: answerCallback)
+    }
+
+    func handle(result: QuizResult<R.Question, R.Answer>) {
+        router.routeTo(result: result)
+    }
+}
+
 private func scoring<Question: Hashable, Answer: Equatable>(_ answers: [Question: Answer], correctAnswers: [Question: Answer]) -> Int {
     return answers.reduce(0) { (score, tuple) in
         return score + (correctAnswers[tuple.key] == tuple.value ? 1 : 0)
