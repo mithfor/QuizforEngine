a11d92639d7f3ccecdf8f52683cda6f37d38a49f
Dmitriy Voronin <devoronin@gmail.com>
2023-07-06 15:38:25 +0300

    Introduced the new Quiz.start APIs

---
+++ QuizforEngineTests/QuizTest.swift
@@ -0,0 +1,82 @@
+//
+//  QuizTest.swift
+//  QuizforEngineTests
+//
+//  Created by Dmitriy Voronin on 06.07.2023.
+//
+
+import Foundation
+import XCTest
+@testable import QuizforEngine
+
+final class Quiz {
+
+    private let flow: Any
+
+    private init(flow: Any) {
+        self.flow = flow
+    }
+
+    static func start<Question: Hashable,
+                          Answer: Equatable,
+                          Delegate: QuizDelegate>(questions: [Question],
+                                     delegate: Delegate,
+                                     correctAnswers: [Question: Answer]) -> Quiz where Delegate.Question == Question, Delegate.Answer ==  Answer {
+        let flow = Flow(questions: questions, delegate: delegate, scoring: { scoring($0, correctAnswers: correctAnswers) })
+        flow.start()
+        return Quiz(flow: flow)
+    }
+}
+
+class QuizTest: XCTestCase {
+    private let delegate = DelegateSpy()
+    private var quiz: Quiz!
+
+    override func setUp() {
+        super.setUp()
+
+        quiz = Quiz.start(questions: ["Q1", "Q2"], delegate: delegate, correctAnswers: ["Q1":"A1", "Q2":"A2" ])
+    }
+
+    func test_startQuiz_answerZeroOutOfTwoCorrectly_scoresZero() {
+        delegate.answerCallback("wrong")
+        delegate.answerCallback("wrong")
+
+        XCTAssertEqual(delegate.handledResult!.score, 0)
+    }
+
+    func test_startQuiz_answerOneOutOfTwoCorrectly_scoresOne() {
+        delegate.answerCallback("A1")
+        delegate.answerCallback("wrong")
+
+        XCTAssertEqual(delegate.handledResult!.score, 1)
+    }
+
+    func test_startQuiz_answerTwoOutOfTwoCorrectly_scoresTwo() {
+        delegate.answerCallback("A1")
+        delegate.answerCallback("A2")
+
+        XCTAssertEqual(delegate.handledResult!.score, 2)
+    }
+
+    private class DelegateSpy: Router, QuizDelegate {
+        func handle(question: String, answerCallback: @escaping (String) -> Void) {
+            self.answerCallback = answerCallback
+        }
+
+        func handle(result: QuizforEngine.QuizResult<String, String>) {
+            handledResult = result
+        }
+
+        var handledResult: QuizResult<String, String>? = nil
+        var answerCallback: ((String) -> Void) = { _ in }
+
+        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+            handle(question: question, answerCallback: answerCallback)
+        }
+
+        func routeTo(result: QuizResult<String, String>) {
+            handle(result: result)
+        }
+    }
+}
