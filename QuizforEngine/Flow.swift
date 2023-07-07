0b685298e3a9c309f26fd5944eb5986c2ce9d4cd
Dmitriy Voronin <devoronin@gmail.com>
2023-07-08 00:33:21 +0300

    Proved the Flow still completes with all received answers when using new APIs

--- QuizforEngine/Flow.swift
+++ QuizforEngine/Flow.swift
@@ -14,6 +14,7 @@ final class Flow<Delegate: QuizDelegate>{
     
     private let delegate: Delegate
     private let questions: [Question]
+    private var newAnswers: [(Question, Answer)] = []
     private var answers: [Question: Answer] = [:]
     private var scoring: ([Question: Answer]) -> Int
     
@@ -36,7 +37,7 @@ final class Flow<Delegate: QuizDelegate>{
             let question = questions[index]
             delegate.answer(for: question, completion: answer(for: question, at: index))
         } else {
-            delegate.didCompleteQuiz(withAnswers: [])
+            delegate.didCompleteQuiz(withAnswers: newAnswers)
             delegate.handle(result: result())
         }
     }
@@ -47,6 +48,7 @@ final class Flow<Delegate: QuizDelegate>{
 
     private func answer(for question: Question, at index: Int) -> (Answer) -> Void {
         return { [weak self] answer in
+            self?.newAnswers.append((question, answer))
             self?.answers[question] = answer
             self?.delegateQuestionHandling(after: index)
         }
