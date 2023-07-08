0d6b9d6df357298c1b487ed81227094fa85cd62b
Dmitriy Voronin <devoronin@gmail.com>
2023-07-08 11:03:29 +0300

    Renaming QuizResult to Result in App and AppTest

--- QuizforEngine/QuizDelegate.swift
+++ QuizforEngine/QuizDelegate.swift
@@ -11,7 +11,6 @@ public protocol QuizDelegate {
     associatedtype Question
     associatedtype Answer
 
-
     func answer(for question: Question, completion: @escaping (Answer) -> Void)
 
     func didCompleteQuiz(withAnswers: [(question: Question, answer: Answer)])
