c80fbbeea2bf1a7d40a29e8df92aa5e5ef478c4c
Dmitriy Voronin <devoronin@gmail.com>
2023-07-25 08:34:13 +0300

    Reduce code duplication with helper factory method

--- QuizforEngineTests/QuizTest.swift
+++ QuizforEngineTests/QuizTest.swift
@@ -14,8 +14,7 @@ class QuizTest: XCTestCase {
 
     func test_startQuiz_answersAllQuestions_completesWithAnswers() {
 
-        let delegate = DelegateSpy()
-        let dataSource = DataSourceSpy()
+        let (delegate, dataSource) = makeSpy()
 
         quiz = Quiz.start(questions: ["Q1", "Q2"], delegate: delegate, dataSource: dataSource)
         dataSource.answerCompletions[0]("A1")
@@ -23,5 +22,28 @@ class QuizTest: XCTestCase {
 
         XCTAssertEqual(delegate.completedQuizzes.count, 1)
         assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
+    }
+//
+//    func test_startQuiz_answersAllQuestionsTwice_completesWithAnswers() {
+//
+//        let (delegate, dataSource) = makeSpy()
+//
+//        quiz = Quiz.start(questions: ["Q1", "Q2"], delegate: delegate, dataSource: dataSource)
+//        dataSource.answerCompletions[0]("A1")
+//        dataSource.answerCompletions[1]("A2")
+//
+//        dataSource.answerCompletions[0]("A1-1")
+//        dataSource.answerCompletions[1]("A2-2")
+//
+//        XCTAssertEqual(delegate.completedQuizzes.count, 2)
+//
+//        assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
+//        assertEqual(delegate.completedQuizzes[1], [("Q1", "A1-1"), ("Q2", "A2-2")])
+//    }
+
+    // MARK: - Helpers
+
+    private func makeSpy() -> (delegate: DelegateSpy, dataSource: DataSourceSpy) {
+        (DelegateSpy(), DataSourceSpy())
     }
 }
